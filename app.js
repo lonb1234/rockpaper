286130ee02e2c89cb76e3dd13550dd01ace67a03
Lon <lonbouweriks@gmail.com>
2020-03-30 11:16:49 +0200

    added functionality to it, all finished except media viewports

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ const jsdom = require("jsdom")
 const browserify = require("browserify")
 const ejs=require("ejs")
 const app = express();
-
 app.set('view engine', 'ejs')
+var winstatus=""
 
 const curl = require("curl");
-
-
+var randomNumber=""
 var score1=0
 
-
+var npcpick=""
 var JSDOM = jsdom.JSDOM;
 global.document = new JSDOM("/index.html").window.document;
 
@@ -33,42 +32,53 @@ app.get("/", function(req, res) {
 
       logEventV=($(this).attr('value'));
       $(h5).html(logEventV)
-      // $(h5).value(logEventV)
       alert(logEventV)
       $(selectchoice).val(logEventV)
-      // alert($(h5).html())
-
-      // $(points).html(+1)
         $('#game').submit();
   })
 
   $(".again").on("click",function(){
+
     var scoreX = ($(scorez).text())
     $(points).val(scoreX)
     $(scorez).html(2)
   })
 
-score1=score1+1
+// score1=score1+1
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res) {
-
-   // if (req.param('action') == '/play') {
-   //   console.log("post 2")
-   //   var points2=(req.body.points);
-   //   console.log(points2)
-   //     // res.sendFile(__dirname + "/index")
-   //   }
-   //
-   // else{
   console.log("post received")
 
-score1=score1+1
-      var num2=(req.body.selectchoice);
-      console.log(num2)
 
-    res.render("play",{score1: score1, num2: num2})
+
+
+// score1=score1+1
+      var num2=(req.body.selectchoice);
+
+      var choices1=['rock','paper', 'scissors', 'lizard', 'spock']
+      npcpick=choices1[Math.floor((Math.random()*5))]
+      if (num2=="rock" && npcpick=="lizard" || num2=="rock" && npcpick=="scissors"||
+          num2=="paper" &&  npcpick=="rock" || num2=="paper" && npcpick=="spock"||
+          num2=="scissors" && npcpick=="paper" || num2=="scissors" && npcpick=="lizard"||
+          num2=="spock" && npcpick=="scissors" || num2=="spock" && npcpick=="rock"||
+          num2=="lizard" && npcpick=="spock" || num2=="lizard" && npcpick=="paper"){
+        score1=score1+1
+        winstatus="Player Won!"
+      }
+      else if(num2=="rock" && npcpick=="paper" || num2=="rock" && npcpick=="spock"||
+              num2=="paper" && npcpick=="scissors" || num2=="paper" && npcpick=="lizard"||
+              num2=="scissors" && npcpick=="rock" || num2=="scissors" && npcpick=="spock"||
+              num2=="spock" && npcpick=="lizard" || num2=="spock" && npcpick=="paper"||
+              num2=="lizard" && npcpick=="rock" || num2=="lizard" && npcpick=="scissors"){
+      score1=score1-1
+      winstatus="Player Lost!"
+      }
+      else{
+      winstatus="It's A Draw!"
+      }
+    res.render("play",{score1: score1, num2: num2, npcpick: npcpick, winstatus:winstatus})
   });
 
 
@@ -79,16 +89,7 @@ score1=score1+1
 
 
 res.render("index0",{score1: score1})
-      // res.sendFile(__dirname + "/")
 console.log(points2)
-      function parseData(html) {
-        const {
-          JSDOM
-        } = jsdom;
-        const dom = new JSDOM(html);
-        const $ = (require('jquery'))(dom.window);
-      $(selectchoice).val("3")
-        }
 
 });
 
